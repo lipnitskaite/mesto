e44e83af08a94d15eb39ba03d761a2b169c6b299
Karolina Lipnitskaite <lipnitskaite@gmail.com>
2021-11-18 15:52:36 +0300

    fix: change styles for like-button, correct scripts, add text-overflow for titles

--- scripts/index.js
+++ scripts/index.js
@@ -1,25 +1,22 @@
 const editLink = document.querySelector('.profile__edit-button');
 const popup = document.querySelector('.popup');
 const popupCloseButton = popup.querySelector('.popup__close');
+const formElement = document.querySelector('.form');
+const nameInput = document.querySelector('.form__item_type_name');
+const jobInput = document.querySelector('.form__item_type_job');
+const profileTitle = document.querySelector('.profile__title');
+const profileSubtitle = document.querySelector('.profile__subtitle');
+const submit = document.querySelector('.form__item_type_submit');
 
 function open () {
-  popup.classList.add('popup__opened');
+  popup.classList.add('popup_opened');
+  nameInput.value = profileTitle.textContent;
+  jobInput.value = profileSubtitle.textContent;
 }
-
-editLink.addEventListener('click', open);
 
 function close () {
-  popup.classList.remove('popup__opened');
+  popup.classList.remove('popup_opened');
 }
-
-popupCloseButton.addEventListener('click', close);
-
-let formElement = document.querySelector('.form');
-let nameInput = document.querySelector('.form__item_type_name');
-let jobInput = document.querySelector('.form__item_type_job');
-let profileTitle = document.querySelector('.profile__title');
-let profileSubtitle = document.querySelector('.profile__subtitle');
-let submit = document.querySelector('.form__item_type_submit');
 
 function formSubmitHandler (evt) {
   evt.preventDefault(); 
@@ -30,6 +27,6 @@ function formSubmitHandler (evt) {
   close();
 }
 
+editLink.addEventListener('click', open);
+popupCloseButton.addEventListener('click', close);
 formElement.addEventListener('submit', formSubmitHandler);
-
-// submit.addEventListener('click', close);
