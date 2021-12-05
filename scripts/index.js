d5bb48d03bfd87d953ad8c05b3a654d5b48ddbab
Karolina Lipnitskaite <lipnitskaite@gmail.com>
2021-12-05 19:52:33 +0300

    fix: fix new-item form and add transparent background for close button

--- scripts/index.js
+++ scripts/index.js
@@ -1,4 +1,25 @@
-// Cards with pictures
+// Edit profile form
+const editLink = document.querySelector('.profile__edit-button');
+const popupEditProfile = document.querySelector('.popup_type_edit-profile');
+const popupEditCloseButton = popupEditProfile.querySelector('.popup__close');
+const addLink = document.querySelector('.profile__add-button');
+const formElement = document.querySelector('.form');
+const nameInput = document.querySelector('.form__item_type_name');
+const jobInput = document.querySelector('.form__item_type_job');
+const profileTitle = document.querySelector('.profile__title');
+const profileSubtitle = document.querySelector('.profile__subtitle');
+
+// Add post form
+const popupAddPost = document.querySelector('.popup_type_add-post');
+const popupAddCloseButton = popupAddPost.querySelector('.popup__close');
+const postTitle = popupAddPost.querySelector('.form__item_type_post-title');
+const postImage = popupAddPost.querySelector('.form__item_type_post-image');
+const submit = document.querySelector('.form__item_type_submit');
+
+// Like button
+// const places = document.querySelector('.places').cloneNode(true);
+
+// Initial cards with pictures
 const initialCards = [
   {
     name: 'Алтайский край',
@@ -55,25 +76,7 @@ function getItem(item) {
 
 render();
 
-// Edit profile form
-const editLink = document.querySelector('.profile__edit-button');
-const popupEditProfile = document.querySelector('.popup_type_edit-profile');
-const popupEditCloseButton = popupEditProfile.querySelector('.popup__close');
-const addLink = document.querySelector('.profile__add-button');
-const formElement = document.querySelector('.form');
-const nameInput = document.querySelector('.form__item_type_name');
-const jobInput = document.querySelector('.form__item_type_job');
-const profileTitle = document.querySelector('.profile__title');
-const profileSubtitle = document.querySelector('.profile__subtitle');
-// Add post form
-const popupAddPost = document.querySelector('.popup_type_add-post');
-const popupAddCloseButton = popupAddPost.querySelector('.popup__close');
-const submit = document.querySelector('.form__item_type_submit');
-// Like button
-const places = document.querySelector('.places').cloneNode(true);
-
 // Open popupEditProfile
-
 function openPopupEdit () {
   popupEditProfile.classList.add('popup_opened');
   nameInput.value = profileTitle.textContent;
@@ -83,7 +86,6 @@ function openPopupEdit () {
 editLink.addEventListener('click', openPopupEdit);
 
 // Submit popupEditProfile changes
-
 function formSubmitHandler (evt) {
   evt.preventDefault(); 
     
@@ -96,17 +98,13 @@ function formSubmitHandler (evt) {
 formElement.addEventListener('submit', formSubmitHandler);
 
 // Open popupAddPost
-
 function openPopupAdd () {
   popupAddPost.classList.add('popup_opened');
-  nameInput.value = profileTitle.textContent;
-  jobInput.value = profileSubtitle.textContent;
 }
 
 addLink.addEventListener('click', openPopupAdd);
 
 // Close popup
-
 function close () {
   popupEditProfile.classList.remove('popup_opened');
   popupAddPost.classList.remove('popup_opened');
@@ -114,6 +112,7 @@ function close () {
 
 popupEditCloseButton.addEventListener('click', close);
 popupAddCloseButton.addEventListener('click', close);
+
 
 // Add like to a place
 // places.querySelector('.place__like-button').addEventListener('click', function (evt) {
