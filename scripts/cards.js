292323ade7455bc38de55d2b1c925b5f93898a8d
Karolina Lipnitskaite <lipnitskaite@gmail.com>
2021-12-08 16:56:13 +0300

    refactor: add cards block into a new file

---
+++ scripts/cards.js
@@ -0,0 +1,33 @@
+// Initial cards 
+const initialCards = [
+  {
+    name: 'Алтайский край',
+    imageSource: './images/altai.jpg',
+    imageAlt: 'Сосновый лес на фоне заснеженных гор'
+  },
+  {
+    name: 'Озеро Байкал',
+    imageSource: './images/baikal-lake.jpg',
+    imageAlt: 'Машина на замерзшем озере на фоне холмистого берега'
+  },
+  {
+    name: 'Кавказ',
+    imageSource: './images/caucasus.jpg',
+    imageAlt: 'Летний вид на горы Кавказа'
+  },
+  {
+    name: 'Гора Эльбрус',
+    imageSource: './images/elbrus-mountain.jpg',
+    imageAlt: 'Осенний вид на Эльбрус'
+  },
+  {
+    name: 'Камчатка',
+    imageSource: './images/kamchatka.jpg',
+    imageAlt: 'Каменнистые горы посреди моря'
+  },
+  {
+    name: 'Куршская Коса',
+    imageSource: './images/kurshskaya-kosa.jpg',
+    imageAlt: 'Деревянная дорога посреди песчаных дюн'
+  }
+];
