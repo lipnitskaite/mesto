d8bb0531317c2837cb3ab0114f3bf9f999c37fde
Karolina Lipnitskaite <lipnitskaite@gmail.com>
2022-01-11 19:56:17 +0300

    set event listeners for buttons for Card

--- scripts/Card.js
+++ scripts/Card.js
@@ -18,12 +18,26 @@ class Card {
 
   generateCard() {
     this._element = this._getTemplate();
+    this._setEventListenersForButtons();
   
     this._element.querySelector('.place__image').src = this._imageSource;
     this._element.querySelector('.place__image').alt = this._imageAlt;
     this._element.querySelector('.place__title').textContent = this._name;
   
     return this._element;
+  }
+
+  _setEventListenersForButtons() {
+    this._element.querySelector('.place__like-button').addEventListener('click', this._handleLike);
+    this._element.querySelector('.place__delete-button').addEventListener('click', this._handleDelete);
+  }
+
+  _handleLike = () => {
+    this._element.querySelector('.place__like-button').classList.toggle('place__like-button_active');
+  }
+
+  _handleDelete = () => {  
+    this._element.remove();
   }
 }
 
